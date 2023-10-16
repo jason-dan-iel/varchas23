7c8abeb21588fe7a1390e61441611a90c7d9864f
Jason Daniel <jasondaniel18@gmail.com>
2023-10-16 18:51:43 +0530

    front page fix2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "./Hero";
 import About from "./About";
 import Gallery from "./Gallery";
 import Soch from "./Soch";
+import Footer from "../components/Footer";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Soch />
             <About />
             <Gallery />
+            <Footer />
         </main>
     )
 }
